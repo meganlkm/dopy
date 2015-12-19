69e7c3b9be246640d07e51bd19ec85f5bfae1fc7
Megan <megan.lkm@gmail.com>
2015-12-19 10:36:15 -0600

    move stuff

--- dopy/api/v2.py
+++ dopy/api/v2.py
@@ -8,13 +8,51 @@ and returns their response as a dict.
 import os
 import sys
 import pprint
-import requests
+from requests import codes, RequestException
+from dopy import common as c
+from dopy.exceptions import DoError
 
-API_ENDPOINT = 'https://api.digitalocean.com/v1'
+API_ENDPOINT = 'https://api.digitalocean.com/v2'
+
+REQUEST_METHODS = {
+    'POST': c.post_request,
+    'PUT': c.put_request,
+    'DELETE': c.delete_request,
+    'GET': c.get_request,
+}
 
 
-class DoError(RuntimeError):
-    pass
+def request_v2(url, headers=None, params=None, timeout=60, method='GET'):
+    if method not in REQUEST_METHODS.keys():
+        raise DoError('Unsupported method %s' % method)
+
+    request_args = {
+        'url': url,
+        'headers': headers,
+        'params': params,
+        'timeout': timeout
+    }
+
+    try:
+        resp = REQUEST_METHODS[method](**request_args)
+        response = resp.json()
+    except ValueError:
+        raise ValueError("The API server doesn't respond with a valid json")
+    except RequestException as e:
+        raise RuntimeError(e)
+
+    if resp.status_code != codes.ok:
+        if response:
+            if 'error_message' in response:
+                raise DoError(response['error_message'])
+            elif 'message' in response:
+                raise DoError(response['message'])
+        # The JSON reponse is bad, so raise an exception with the HTTP status
+        resp.raise_for_status()
+
+    if response.get('id') == 'not_found':
+        raise DoError(response['message'])
+    return response
 
 
 class DoManager(object):
@@ -27,106 +65,127 @@ class DoManager(object):
 
     def all_active_droplets(self):
         json = self.request('/droplets/')
+        for index in range(len(json['droplets'])):
+            self.populate_droplet_ips(json['droplets'][index])
         return json['droplets']
 
     def new_droplet(self, name, size_id, image_id, region_id,
                     ssh_key_ids=None, virtio=True, private_networking=False,
                     backups_enabled=False, user_data=None, ipv6=False):
+
         params = {
             'name': str(name),
-            'size_id': str(size_id),
-            'image_id': str(image_id),
-            'region_id': str(region_id),
+            'size': str(size_id),
+            'image': str(image_id),
+            'region': str(region_id),
             'virtio': str(virtio).lower(),
+            'ipv6': str(ipv6).lower(),
             'private_networking': str(private_networking).lower(),
-            'backups_enabled': str(backups_enabled).lower(),
+            'backups': str(backups_enabled).lower(),
         }
         if ssh_key_ids:
-            # Need to be a comma separated string
-            if type(ssh_key_ids) == list:
-                ssh_key_ids = ','.join(ssh_key_ids)
-            params['ssh_key_ids'] = ssh_key_ids
+            # Need to be an array in v2
+            if isinstance(ssh_key_ids, basestring):
+                ssh_key_ids = [ssh_key_ids]
 
-        json = self.request('/droplets/new', params=params)
-        return json['droplet']
+            if type(ssh_key_ids) == list:
+                for index in range(len(ssh_key_ids)):
+                    ssh_key_ids[index] = str(ssh_key_ids[index])
+
+            params['ssh_keys'] = ssh_key_ids
+
+        if user_data:
+            params['user_data'] = user_data
+
+        json = self.request('/droplets', params=params, method='POST')
+        created_id = json['droplet']['id']
+        json = self.show_droplet(created_id)
+        return json
 
     def show_droplet(self, droplet_id):
         json = self.request('/droplets/%s' % droplet_id)
+        self.populate_droplet_ips(json['droplet'])
         return json['droplet']
 
+    def droplet_v2_action(self, droplet_id, droplet_type, params=None):
+        if params is None:
+            params = {}
+        params['type'] = droplet_type
+        json = self.request('/droplets/%s/actions' % droplet_id, params=params, method='POST')
+        return json
+
     def reboot_droplet(self, droplet_id):
-        json = self.request('/droplets/%s/reboot/' % droplet_id)
+        json = self.droplet_v2_action(droplet_id, 'reboot')
         json.pop('status', None)
         return json
 
     def power_cycle_droplet(self, droplet_id):
-        json = self.request('/droplets/%s/power_cycle/' % droplet_id)
+        json = self.droplet_v2_action(droplet_id, 'power_cycle')
         json.pop('status', None)
         return json
 
     def shutdown_droplet(self, droplet_id):
-        json = self.request('/droplets/%s/shutdown/' % droplet_id)
+        json = self.droplet_v2_action(droplet_id, 'shutdown')
         json.pop('status', None)
         return json
 
     def power_off_droplet(self, droplet_id):
-        json = self.request('/droplets/%s/power_off/' % droplet_id)
+        json = self.droplet_v2_action(droplet_id, 'power_off')
         json.pop('status', None)
         return json
 
     def power_on_droplet(self, droplet_id):
-        json = self.request('/droplets/%s/power_on/' % droplet_id)
+        json = self.droplet_v2_action(droplet_id, 'power_on')
         json.pop('status', None)
         return json
 
     def password_reset_droplet(self, droplet_id):
-        json = self.request('/droplets/%s/password_reset/' % droplet_id)
+        json = self.droplet_v2_action(droplet_id, 'password_reset')
         json.pop('status', None)
         return json
 
     def resize_droplet(self, droplet_id, size_id):
-        params = {'size_id': size_id}
-        json = self.request('/droplets/%s/resize/' % droplet_id, params)
+        params = {'size': size_id}
+        json = self.droplet_v2_action(droplet_id, 'resize', params)
         json.pop('status', None)
         return json
 
     def snapshot_droplet(self, droplet_id, name):
         params = {'name': name}
-        json = self.request('/droplets/%s/snapshot/' % droplet_id, params)
+        json = self.droplet_v2_action(droplet_id, 'snapshot', params)
         json.pop('status', None)
         return json
 
     def restore_droplet(self, droplet_id, image_id):
-        params = {'image_id': image_id}
-        json = self.request('/droplets/%s/restore/' % droplet_id, params)
+        params = {'image': image_id}
+        json = self.droplet_v2_action(droplet_id, 'restore', params)
         json.pop('status', None)
         return json
 
     def rebuild_droplet(self, droplet_id, image_id):
-        params = {'image_id': image_id}
-        json = self.request('/droplets/%s/rebuild/' % droplet_id, params)
+        params = {'image': image_id}
+        json = self.droplet_v2_action(droplet_id, 'rebuild', params)
         json.pop('status', None)
         return json
 
     def enable_backups_droplet(self, droplet_id):
-        json = self.request('/droplets/%s/enable_backups/' % droplet_id)
+        json = self.droplet_v2_action(droplet_id, 'enable_backups')
         json.pop('status', None)
         return json
 
     def disable_backups_droplet(self, droplet_id):
-        json = self.request('/droplets/%s/disable_backups/' % droplet_id)
+        json = self.droplet_v2_action(droplet_id, 'disable_backups')
         json.pop('status', None)
         return json
 
     def rename_droplet(self, droplet_id, name):
         params = {'name': name}
-        json = self.request('/droplets/%s/rename/' % droplet_id, params)
+        json = self.droplet_v2_action(droplet_id, 'rename', params)
         json.pop('status', None)
         return json
 
     def destroy_droplet(self, droplet_id, scrub_data=True):
-        params = {'scrub_data': '1' if scrub_data else '0'}
-        json = self.request('/droplets/%s/destroy/' % droplet_id, params)
+        json = self.request('/droplets/%s' % droplet_id, method='DELETE')
         json.pop('status', None)
         return json
 
@@ -151,46 +210,52 @@ class DoManager(object):
         return json['images']
 
     def private_images(self):
-        params = {'filter': 'my_images'}
-        json = self.request('/images/', params)
+        json = self.request('/images?private=true')
         return json['images']
 
+    def image_v2_action(self, image_id, image_type, params=None):
+        if params is None:
+            params = {}
+        params['type'] = image_type
+        json = self.request('/images/%s/actions' % image_id, params=params, method='POST')
+        return json
+
     def show_image(self, image_id):
-        params = {'image_id': image_id}
         json = self.request('/images/%s' % image_id)
         return json['image']
 
     def destroy_image(self, image_id):
-        self.request('/images/%s/destroy' % image_id)
+        self.request('/images/%s' % image_id, method='DELETE')
         return True
 
     def transfer_image(self, image_id, region_id):
-        params = {'region_id': region_id}
-        json = self.request('/images/%s/transfer' % image_id, params)
+        params = {'region': region_id}
+        json = self.image_v2_action(image_id, 'transfer', params)
         json.pop('status', None)
         return json
 
     # ssh_keys=========================================
     def all_ssh_keys(self):
-        json = self.request('/ssh_keys/')
+        json = self.request('/account/keys')
         return json['ssh_keys']
 
     def new_ssh_key(self, name, pub_key):
-        params = {'name': name, 'ssh_pub_key': pub_key}
-        json = self.request('/ssh_keys/new/', params)
+        params = {'name': name, 'public_key': pub_key}
+        json = self.request('/account/keys', params, method='POST')
         return json['ssh_key']
 
     def show_ssh_key(self, key_id):
-        json = self.request('/ssh_keys/%s/' % key_id)
+        json = self.request('/account/keys/%s/' % key_id)
         return json['ssh_key']
 
     def edit_ssh_key(self, key_id, name, pub_key):
-        params = {'name': name, 'ssh_pub_key': pub_key}  # the doc needs to be improved
-        json = self.request('/ssh_keys/%s/edit/' % key_id, params)
+        # v2 API doesn't allow to change key body now
+        params = {'name': name}
+        json = self.request('/account/keys/%s/' % key_id, params, method='PUT')
         return json['ssh_key']
 
     def destroy_ssh_key(self, key_id):
-        self.request('/ssh_keys/%s/destroy/' % key_id)
+        self.request('/account/keys/%s' % key_id, method='DELETE')
         return True
 
     # sizes============================================
@@ -205,7 +270,7 @@ class DoManager(object):
 
     def new_domain(self, name, ip):
         params = {'name': name, 'ip_address': ip}
-        json = self.request('/domains/new/', params)
+        json = self.request('/domains', params=params, method='POST')
         return json['domain']
 
     def show_domain(self, domain_id):
@@ -213,17 +278,17 @@ class DoManager(object):
         return json['domain']
 
     def destroy_domain(self, domain_id):
-        self.request('/domains/%s/destroy/' % domain_id)
+        self.request('/domains/%s' % domain_id, method='DELETE')
         return True
 
     def all_domain_records(self, domain_id):
         json = self.request('/domains/%s/records/' % domain_id)
-        return json['records']
+        return json['domain_records']
 
     def new_domain_record(self, domain_id, record_type, data, name=None,
                           priority=None, port=None, weight=None):
         params = {'data': data}
-        params['record_type'] = record_type
+        params['type'] = record_type
 
         if name:
             params['name'] = name
@@ -234,85 +299,50 @@ class DoManager(object):
         if weight:
             params['weight'] = weight
 
-        json = self.request('/domains/%s/records/new/' % domain_id, params)
-        return json['record']
+        json = self.request('/domains/%s/records/' % domain_id, params, method='POST')
+        return json['domain_record']
 
     def show_domain_record(self, domain_id, record_id):
         json = self.request('/domains/%s/records/%s' % (domain_id, record_id))
-        return json['record']
+        return json['domain_record']
 
     def edit_domain_record(self, domain_id, record_id, record_type, data,
                            name=None, priority=None, port=None, weight=None):
-        params = {
-            'record_type': record_type,
-            'data': data,
-        }
-
-        if name:
-            params['name'] = name
-        if priority:
-            params['priority'] = priority
-        if port:
-            params['port'] = port
-        if weight:
-            params['weight'] = weight
-        json = self.request('/domains/%s/records/%s/edit/' % (domain_id, record_id), params)
-        return json['record']
+        # API v.2 allows only record name change
+        params = {'name': name}
+        json = self.request('/domains/%s/records/%s' % (domain_id, record_id), params, method='PUT')
+        return json['domain_record']
 
     def destroy_domain_record(self, domain_id, record_id):
-        self.request('/domains/%s/records/%s/destroy/' % (domain_id, record_id))
+        self.request('/domains/%s/records/%s' % (domain_id, record_id), method='DELETE')
         return True
 
     # events(actions in v2 API)========================
     def show_all_actions(self):
-        # API v.1 haven't this functionality
-        return False
+        json = self.request('/actions')
+        return json['actions']
 
     def show_action(self, action_id):
-        return self.show_event(action_id)
+        json = self.request('/actions/%s' % action_id)
+        return json['action']
 
     def show_event(self, event_id):
-        json = self.request('/events/%s' % event_id)
-        return json['event']
+        return self.show_action(event_id)
 
     # low_level========================================
     def request(self, path, params={}, method='GET'):
         if not path.startswith('/'):
             path = '/' + path
-        url = self.api_endpoint + path
+        url = API_ENDPOINT + path
 
-        params['client_id'] = self.client_id
-        params['api_key'] = self.api_key
-        resp = self.request_v1(url, params, method=method)
-        return resp
-
-    def request_v1(self, url, params={}, method='GET'):
-        try:
-            resp = requests.get(url, params=params, timeout=60)
-            json = resp.json()
-        except ValueError:  # requests.models.json.JSONDecodeError
-            raise ValueError("The API server doesn't respond with a valid json")
-        except requests.RequestException as e:  # errors from requests
-            raise RuntimeError(e)
-
-        if resp.status_code != requests.codes.ok:
-            if json:
-                if 'error_message' in json:
-                    raise DoError(json['error_message'])
-                elif 'message' in json:
-                    raise DoError(json['message'])
-            # The JSON reponse is bad, so raise an exception with the HTTP status
-            resp.raise_for_status()
-        if json.get('status') != 'OK':
-            raise DoError(json['error_message'])
-
-        return json
+        headers = {'Authorization': "Bearer %s" % self.api_key}
+        return request_v2(url, params=params, headers=headers, timeout=60, method=method)
 
 
 if __name__ == '__main__':
-    client_id = os.environ['DO_CLIENT_ID']
-    api_key = os.environ['DO_API_KEY']
-    do = DoManager(client_id, api_key, 1)
+    api_token = os.environ.get('DO_API_TOKEN') or os.environ['DO_API_KEY']
+    print "api_token: {}".format(api_token)
+    do = DoManager(None, api_token, 2)
 
     fname = sys.argv[1]
 
---
+++ dopy/common.py
@@ -0,0 +1,84 @@
+import json
+import requests
+from six import wraps
+
+
+class MockResponse(object):
+    def __init__(self, method, url, params, headers, timeout):
+        self.url = url
+        self.method = method
+        self.params = params
+        self.headers = headers
+        self.timeout = timeout
+        self.status_code = 200
+
+    def json(self):
+        return {'url': self.url, 'id': '12345',
+                'method': self.method, 'params': self.params,
+                'headers': self.headers, 'timeout': self.timeout,
+                'droplets': []}
+
+
+def _compile_request_args(params, headers, timeout):
+    kwargs = {
+        'headers': {} if headers is None else headers,
+        'params': {} if params is None else params,
+        'timeout': int(timeout)
+    }
+    kwargs['headers']['Content-Type'] = 'application/json'
+    return kwargs
+
+
+def paginated(func):
+    @wraps(func)
+    def wrapper(url, headers=None, params=None, timeout=60):
+        nxt = url
+        out = {}
+
+        while nxt is not None:
+            result = func(nxt, headers, params, 'GET')
+            nxt = None
+
+            if isinstance(result, dict):
+                for key, value in result.items():
+                    if key in out and isinstance(out[key], list):
+                        out[key].extend(value)
+                    else:
+                        out[key] = value
+
+                if 'links' in result \
+                        and 'pages' in result['links'] \
+                        and 'next' in result['links']['pages']:
+                    nxt = result['links']['pages']['next']
+
+        return out
+    return wrapper
+
+
+def post_request(url, params=None, headers=None, timeout=60):
+    kwargs = _compile_request_args(params, headers, timeout)
+    kwargs['data'] = json.dumps(kwargs['params'])
+    del(kwargs['params'])
+    # return requests.post(url, **kwargs)
+    return MockResponse('POST', url, **kwargs)
+
+
+def put_request(url, params=None, headers=None, timeout=60):
+    kwargs = _compile_request_args(params, headers, timeout)
+    # return requests.put(url, **kwargs)
+    return MockResponse('PUT', url, **kwargs)
+
+
+def delete_request(url, params=None, headers=None, timeout=60):
+    kwargs = _compile_request_args(params, headers, timeout)
+    # resp = requests.delete(url, **kwargs)
+    # resp.json = {'status': resp.status_code}
+    # return resp
+    return MockResponse('DELETE', url, **kwargs)
+
+
+# @paginated
+def get_request(url, params=None, headers=None, timeout=60):
+    kwargs = _compile_request_args(params, headers, timeout)
+    return requests.get(url, **kwargs)
+    # return MockResponse('GET', url, **kwargs)
